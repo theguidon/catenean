8eba3f07b9cc4c2eed850c8272c063f8ecf42b73
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-05-08 01:27:24 +0800

    Added the Header and Mail and Credit Logos

--- src/assets/Map/Map.jsx
+++ src/assets/Map/Map.jsx
@@ -1,8 +1,24 @@
-import styles from "./Map.css";
+import "./Map.css";
+import Credits_Logo from "../Map/Map Images/Credits_Logo.svg";
+import Mail_Logo from "../Map/Map Images/Mail_Logo.svg";
+import Header from "../Map/Map Images/Header.svg";
 
 function Map(){
     return(
-        <h1>Welcome to the Map</h1>
+        <>
+            <div className="upperLogosWrapper">
+                <span className="Mail_Logo_Container">
+                    <img className="Mail_Logo" src={Mail_Logo} alt="" />
+                </span>
+                <span className="Credits_Logo_Container">
+                    <img className="Credits_Logo" src={Credits_Logo} alt="" />
+                </span>
+            </div>
+            <div className="headerWrapper">
+                <img className="Header" src={Header}/>
+            </div>
+        </>
+        
     );
 }
 

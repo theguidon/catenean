507a862600b5553bf3af156d635ca071e4a728a7
Chesca <fdjr401@gmail.com>
2024-05-09 21:56:14 +0800

    Add location writeup component

---
+++ src/components/LocationWriteup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/LocationWriteup.css"
+
+export default function LocationWriteup() {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="content__photo-set">
+          <div className="content__photo">
+            photo
+          </div>
+          <div className="content__photo">
+            photo
+          </div>
+        </div>
+        <div className="content__writing">
+          for the writing
+        </div>
+      </div>
+    </div>
+  )
+}

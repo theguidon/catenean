0bdb62f06be7f2a6887838ea5bc7115ba92fb8ca
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-05-12 02:03:07 +0800

    Map background has been added

--- src/assets/Map/Map.jsx
+++ src/assets/Map/Map.jsx
@@ -1,24 +1,26 @@
 import "./Map.css";
-import Credits_Logo from "../Map/Map Images/Credits_Logo.svg";
-import Mail_Logo from "../Map/Map Images/Mail_Logo.svg";
-import Header from "../Map/Map Images/Header.svg";
+import Background from "../Map/Map_Images/Map_background.svg";
+import Credits_Logo from "../Map/Map_Images/Credits_Logo.svg";
+import Mail_Logo from "../Map/Map_Images/Mail_Logo.svg";
+import Header from "../Map/Map_Images/Header.svg";
 
 function Map(){
     return(
         <>
-            <div className="upperLogosWrapper">
-                <span className="Mail_Logo_Container">
-                    <img className="Mail_Logo" src={Mail_Logo} alt="" />
-                </span>
-                <span className="Credits_Logo_Container">
-                    <img className="Credits_Logo" src={Credits_Logo} alt="" />
-                </span>
-            </div>
-            <div className="headerWrapper">
-                <img className="Header" src={Header}/>
+            <div className="backgroundWrapper">
+                <div className="upperLogosWrapper">
+                    <span className="Mail_Logo_Container">
+                        <img className="Mail_Logo" src={Mail_Logo} alt="" />
+                    </span>
+                    <span className="Credits_Logo_Container">
+                        <img className="Credits_Logo" src={Credits_Logo} alt="" />
+                    </span>
+                </div>
+                <div className="headerWrapper">
+                    <img className="Header" src={Header}/>
+                </div>
             </div>
         </>
-        
     );
 }
 

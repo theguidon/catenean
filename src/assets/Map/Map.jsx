d5cb340b7aee68593207fbb54c2055adc9ca8424
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-05-12 17:14:09 +0800

    Arete button and Arete cats added

--- src/assets/Map/Map.jsx
+++ src/assets/Map/Map.jsx
@@ -1,8 +1,12 @@
 import "./Map.css";
-import Background from "../Map/Map_Images/Map_background.svg";
+import Map_Campus from "../Map/Map_Images/Map_background.svg";
 import Credits_Logo from "../Map/Map_Images/Credits_Logo.svg";
 import Mail_Logo from "../Map/Map_Images/Mail_Logo.svg";
+import Mail_Logo_Hover from "../Map/Map_Images/Mail_Logo_Hover.svg";
 import Header from "../Map/Map_Images/Header.svg";
+import Cats_Arete from "../Map/Map_Images/Cats_Arete.svg";
+import Button_Arete from "../Map/Map_Images/Button_Arete.svg";
+import Button_Arete_Hover from "../Map/Map_Images/Button_Arete_Hover.svg";
 
 function Map(){
     return(
@@ -11,13 +15,19 @@ function Map(){
                 <div className="upperLogosWrapper">
                     <span className="Mail_Logo_Container">
                         <img className="Mail_Logo" src={Mail_Logo} alt="" />
+                        <img className="Mail_Logo_Hover" src={Mail_Logo_Hover} alt="" />
                     </span>
                     <span className="Credits_Logo_Container">
                         <img className="Credits_Logo" src={Credits_Logo} alt="" />
                     </span>
                 </div>
-                <div className="headerWrapper">
-                    <img className="Header" src={Header}/>
+                <div className="mapWrapper">
+                    <div className="headerWrapper">
+                        <img className="Header" src={Header}/>
+                    </div>
+                    <img className="Map_Campus" src={Map_Campus} alt="" />
+                    <img className="Button_Arete" src={Button_Arete} alt="" />
+                    <img className="Cats_Arete" src={Cats_Arete} alt="" />
                 </div>
             </div>
         </>

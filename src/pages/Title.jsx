7f80734315529f0fa8ba6862eebc11274579dc09
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-05-24 12:54:45 +0800

    fixed text layout

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -3,10 +3,10 @@ import "../stylesheets/title.css";
 function Title() {
   return (
     <section className="title">
-      <h2 className="subtitle-text">What the</h2>
-      <h1 className="title-text">Cateneans</h1>
-      <h2 className="subtitle-text">teach us about love</h2>
-      <div className="btn">
+      <div className="middle-container">
+        <h2 className="subtitle-text">What the</h2>
+        <h1 className="title-text">Cateneans</h1>
+        <h2 className="subtitle-text">teach us about love</h2>
         <div className="start-btn">Start</div>
       </div>
     </section>

c64c79d6c45dab8ef29eb05caf8b9dbee209f6f6
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-05-26 01:00:27 +0800

    added showcat state in home

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "../styles/title.css";
 import HeartImages from "../components/HeartImages";
 import HomeCat from "../assets/images/home-cat.png";
+
 function Title() {
+  const [showCat, setShowCat] = useState(false);
+
   return (
     <section className="title">
       <div className="middle-container">
         <h2 className="subtitle-text">What the</h2>
         <h1 className="title-text">Cateneans</h1>
         <h2 className="subtitle-text">teach us about love</h2>
-        <div className="start-btn">Start</div>
-        <img className="home-cat" src={HomeCat} alt="Home Cat" />
+        <div
+          className="start-btn"
+          onMouseEnter={() => setShowCat(true)}
+          onMouseLeave={() => setShowCat(false)}
+        >
+          Start
+        </div>
+        {showCat ? (
+          <img
+            className={`home-cat ${showCat ? "show" : "hide"}`}
+            src={HomeCat}
+            alt="Home Cat"
+          />
+        ) : (
+          ""
+        )}
       </div>
       <HeartImages />
     </section>

e3f1996dc5484537bb3d7c078f92f15ec051022d
Waleed Lugod <lugodwaleed@gmail.com>
2024-06-02 02:21:42 +0800

    Added text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Title from "./pages/Title.jsx";
+import Map from "./pages/Map.jsx"
+import ArticleMain from "./pages/ArticleMain.jsx"
 
 function App() {
-  return <Title />;
+  return <ArticleMain />;
 }
 
 export default App;
--- src/pages/Article.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "../styles/Article.module.css"
-const Article = () => {
-	return <main className={styles.main}></main>
-}
-
-export default Article
-

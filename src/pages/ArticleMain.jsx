89b3501f1d720ba6c0033bcb6d8f025478e7b8ea
Waleed Lugod <lugodwaleed@gmail.com>
2024-06-02 04:48:57 +0800

    added credits (missing photos bylines)

--- src/pages/ArticleMain.jsx
+++ src/pages/ArticleMain.jsx
@@ -229,6 +229,30 @@ const ArticleMain = () => {
         acknowledgement of their value as living beings.
       </p>
       <p>With reports by Gabrielle Christina A. Cortes</p>
+      <section className={styles.credits}>
+        <div>
+          <h3>Written by</h3>
+          <p>Isabel&nbsp;Victorino and Alyssa&nbsp;E.&nbsp;Duque</p>
+        </div>
+        <div>
+          <h3>Photos by</h3>
+          <p>need bylines no bylines</p>
+        </div>
+        <div>
+          <h3>Designed by</h3>
+          <p>
+            Bea&nbsp;Gan, Juan&nbsp;Lacanienta, Frants&nbsp;Reyes, and
+            Razel&nbsp;Tan
+          </p>
+        </div>
+        <div>
+          <h3>Developed by</h3>
+          <p>
+            Luigi&nbsp;Rodriguez, Ysabella&nbsp;B.&nbsp;Panghulan,
+            Chesca&nbsp;Reyes, and Waleed&nbsp;Lugod
+          </p>
+        </div>
+      </section>
     </main>
   );
 };

0c17ed0962eda3a7cced9328c54b3d470207ffa0
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-04 22:45:36 +0800

    extra classes for the components styled in the jsx

--- src/components/mainarticle/Conclusion.jsx
+++ src/components/mainarticle/Conclusion.jsx
@@ -1,5 +1,5 @@
 import creditStyles from "../../styles/mainCredits.module.css";
-import letter from "../../assets/images/letter.svg";
+import envelope from "../../assets/images/letter.svg";
 import paopao from "../../assets/images/cats/2.jpeg";
 import back from "../../assets/images/back_to_start.svg";
 
@@ -22,30 +22,10 @@ const Conclusion = () => {
   return (
     <>
       <img
-        src={letter}
-        style={{
-          width: "120%",
-          transform: "rotate(-10deg)",
-          boxShadow: "0px 4px 18px 0 rgba(0,0,0,0.2)",
-          borderRadius: "5%",
-          position: "relative",
-          zIndex: 0
-        }} />
-      <section
-        style={{
-          zIndex: 10,
-          position: "relative",
-          width: "120%",
-          padding: "48px",
-          background: "#f9f4eb",
-          border: "8px solid #faa685",
-          transform: "rotate(5deg)",
-          left: "7%",
-          fontFamily: "Outfit",
-          color: "#522222",
-          boxShadow: "0px 4px 18px 0 rgba(0,0,0,0.2)",
-        }}
-      >
+        src={envelope}
+        className={creditStyles.envelope}
+      />
+      <section className={creditStyles.letter}>
         <p>The Cateneans stand as a testament to the human capacity
           to go above and beyond for animals. Resuello, Beunafe, Johnson, and
           the AGILA members are only among countless people who give the animals

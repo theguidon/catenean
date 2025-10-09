825287c25997607c5c6c1f8f4ee3388dbd209e11
Waleed Lugod <lugodwaleed@gmail.com>
2025-10-09 18:05:52 +0800

    title

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Title.module.css";
 import paw from "../assets/images/paw-brown.svg";
+import title from "../assets/images/title.svg";
 
 function Title() {
   return (
@@ -14,6 +15,11 @@ function Title() {
             {i % 2 == 0 ? <div /> : <></>}
           </div>
         ))}
+      </div>
+      <div className={styles.fg}>
+        <div className={styles.title}>
+          <img src={title} alt="" />
+        </div>
       </div>
     </section>
   );

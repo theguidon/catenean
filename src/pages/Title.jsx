d14b294d92f02988a0a7a09cd9eef7d71cc92d61
Waleed Lugod <lugodwaleed@gmail.com>
2025-10-09 14:59:51 +0800

    bg pattern

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,34 +1,20 @@
-import "../styles/title.css";
-import HeartImages from "../components/HeartImages";
-import GuidonLogo from "../assets/images/guidon.svg";
-import HeartBG from "../assets/images/heart-background.png";
-import PawBG from "../assets/images/paw-background.png";
+import styles from "../styles/Title.module.css";
+import paw from "../assets/images/paw-brown.svg";
 
-import { useState, useEffect } from "react";
-
-function Title({ setPage }) {
-  const [isBgHeart, setIsBgHeart] = useState(true);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsBgHeart(!isBgHeart);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [isBgHeart]);
-
+function Title() {
   return (
-    <section className="title">
-      <div className="middle-container">
-        <img src={GuidonLogo} className="guidon-logo" />
-        <div className="interactive-title">
-          <h2 className="subtitle-text">What the</h2>
-          <h1 className="title-text">Cateneans</h1>
-          <h2 className="subtitle-text">teach us about love</h2>
-        </div>
-        <button className="start-btn" onClick={() => setPage("map")}>Start</button>
+    <section className={styles.main}>
+      <div className={styles.bg}>
+        {[...Array(7).keys()].map((e, i) => (
+          <div>
+            {i % 2 != 0 ? <div /> : <></>}
+            {[...Array(10).keys()].map(() => (
+              <img src={paw} alt="" />
+            ))}
+            {i % 2 == 0 ? <div /> : <></>}
+          </div>
+        ))}
       </div>
-      <img src={isBgHeart ? HeartBG : PawBG} className="title-background" />
-      <HeartImages />
     </section>
   );
 }

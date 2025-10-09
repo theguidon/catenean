5b54957df09ac0971a227e99d105d022da829e5b
Waleed Lugod <lugodwaleed@gmail.com>
2025-10-09 18:33:26 +0800

    add cat house

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -2,6 +2,7 @@ import styles from "../styles/Title.module.css";
 import paw from "../assets/images/paw-brown.svg";
 import title from "../assets/images/title.svg";
 import startButton from "../assets/images/start-button.svg";
+import catHouse from "../assets/images/cat-house.svg";
 
 function Title() {
   return (
@@ -17,10 +18,13 @@ function Title() {
           </div>
         ))}
       </div>
+      <div className={styles.mg}>
+        <img className={styles.catHouse} src={catHouse} alt="" />
+      </div>
       <div className={styles.fg}>
         <div className={styles.title}>
           <img src={title} alt="What the Cateneans Teach Us About Love" />
-          <p>Written by Isabel Victorino and Alyssa E. Duque</p>
+          <p>Written by Isabel&nbsp;Victorino and Alyssa&nbsp;E.&nbsp;Duque</p>
         </div>
         <button>
           <img src={startButton} alt="Start" />

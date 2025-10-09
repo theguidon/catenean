c38b9f4064b4081417d789e910e37270e2676505
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-10-09 20:29:53 +0800

    added cat

--- src/components/mainarticle/ComeAndGoMobile.jsx
+++ src/components/mainarticle/ComeAndGoMobile.jsx
@@ -1,30 +1,6 @@
 import styles from "../../styles/ComeAndGoMobile.module.css";
 import building from "../../assets/images/location-faura-building.svg";
-import squiggle from "../../assets/images/squiggle.svg";
-import MapFold from "../../assets/images/location-map-fold.svg";
-import cat1 from "../../assets/images/cats/2.jpeg";
-import cat2 from "../../assets/images/cats/7.jpeg";
-import cat3 from "../../assets/images/cats/6.jpeg";
-import photoCardsLine from "../../assets/images/photo-cards-line.svg";
-import helloCat from "../../assets/images/hello-cat.png";
-import cloud from "../../assets/images/cloud.svg";
-import heartCatSmall1 from "../../assets/images/heart-cat-small-1.png";
-import heartCatSmall2 from "../../assets/images/heart-cat-small-2.png";
-import heartCatSmall3 from "../../assets/images/heart-cat-small-3.png";
-import heartCatBig from "../../assets/images/heart-cat-big.png";
-import circleCat from "../../assets/images/circle-cat.png";
-import mag1 from "../../assets/images/mag1.png";
-import mag2 from "../../assets/images/mag2.png";
-import heartHighFive from "../../assets/images/heart-high-five.svg";
-import cloudCat from "../../assets/images/cloud-cat.png";
-import bigCloud from "../../assets/images/big-cloud.png";
-import grass from "../../assets/images/grass.png";
-import paopao from "../../assets/images/paopao.png";
-import brownEllipses from "../../assets/images/brown-ellipses.svg";
-import bigHeart from "../../assets/images/big-heart.svg";
-import threeCats from "../../assets/images/three-cats.png";
-import paw from "../../assets/images/paw.svg";
-import heart from "../../assets/images/heart.svg";
+import cat1 from "../../assets/images/cats/17.png";
 
 const ComeAndGo = ({ setPage }) => {
   return (
@@ -49,7 +25,7 @@ const ComeAndGo = ({ setPage }) => {
           them are lucky enough to reach old age, some succumb to sickness, and
           some unfortunately fall victim to human callousness.
         </p>
-
+        <img className={styles.cat1} src={cat1} />
         <p>
           Johnson recounts the day Pao-Pao was run over because of “the reckless
           imprudence of drivers.” He first heard the news of Pao-Pao’s passing

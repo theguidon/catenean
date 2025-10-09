953c2563e509d994a1cca455ea46f1ca500cf61c
Waleed Lugod <lugodwaleed@gmail.com>
2025-10-09 18:15:06 +0800

    start button

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Title.module.css";
 import paw from "../assets/images/paw-brown.svg";
 import title from "../assets/images/title.svg";
+import startButton from "../assets/images/start-button.svg";
 
 function Title() {
   return (
@@ -18,8 +19,12 @@ function Title() {
       </div>
       <div className={styles.fg}>
         <div className={styles.title}>
-          <img src={title} alt="" />
+          <img src={title} alt="What the Cateneans Teach Us About Love" />
+          <p>Written by Isabel Victorino and Alyssa E. Duque</p>
         </div>
+        <button>
+          <img src={startButton} alt="Start" />
+        </button>
       </div>
     </section>
   );

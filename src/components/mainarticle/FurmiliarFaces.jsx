d83bafd058f87682006ff73ef4d69914d823e9a1
Abby Montayre <raphaelleabby.montayre@gmail.com>
2025-10-11 11:41:15 +0800

    Fixed Image Scaling of Grass, Building, and Cat. Added Animations

--- src/components/mainarticle/FurmiliarFaces.jsx
+++ src/components/mainarticle/FurmiliarFaces.jsx
@@ -2,24 +2,29 @@ import sectionStyles from "../../styles/furmiliarFaces.module.css";
 import CTC from "../../assets/images/location-ctc-som-building.svg";
 import Bush from "../../assets/images/bush.svg";
 import Grass from "../../assets/images/grass.svg";
-import HelloCats from "../../assets/images/hello-cat.png";
-const Conclusion = () => {
+import HelloCats from "../../assets/images/cat-speech.svg";
+import QuoteBackground from "../../assets/images/quote-background.svg";
+import { easeInOut, motion, } from "motion/react";
+
+
+const FurmiliarFaces = () => {
 
 return (
     <>
     <main className={sectionStyles.main}>
-        <section>
-            <h3
-            >Fur-miliar faces</h3>
-            <section className={sectionStyles.hero}>
-                <img src={CTC}
-                    className={sectionStyles.house}></img>
+            <h3>Fur-miliar faces</h3>
+                <motion.img src={CTC}
+                            initial={{ opacity: 0, y: -20 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.500, ease: easeInOut }}
+                    className={sectionStyles.building}>
+                </motion.img>
                 <img src={Grass}
                     className={sectionStyles.grass}></img>
                 <img src={Bush}
-                    className={sectionStyles.bush}></img>
-            </section>
-        </section>
+                    className={sectionStyles.bushLeft}></img>
+                <img src={Bush}
+                    className={sectionStyles.bushRight}></img>
         <article>
         <p>
             Jesus “Jhess” Resuello is a humble member of the maintenance staff at the PLDT-Convergent Technologies Center and School of Management (CTC-SOM) buildings. As he starts his day, the sound of his motorcycle is enough to stop the Gonzaga cats in their tracks, who scramble to greet him before he can even park.
@@ -33,21 +38,27 @@ return (
         </article>
         <section className={sectionStyles.helloCats}>
             <img src={HelloCats}></img>
+            <motion.h4
+                initial={{ opacity: 0, y: -10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.500, ease: easeInOut, delay: 0.250 }}>
+                    hello!</motion.h4>
         </section>
         <article className={sectionStyles.quoteSection}>
-            <h2 className={sectionStyles.quote}>"</h2>
+        <img src={QuoteBackground} className={sectionStyles.quoteBackground}></img>
+            <h2 className={sectionStyles.quotatiomarkLeft}>"</h2>
             <p>
                 Medyo nasasaktan ako dahil medyo mabigat [sa bulsa] ‘yung binibili kong pagkain, ‘yung Oc [cat food brand] Php 175 isang kilo. Eh halos minsan isang kilo, [k]aunti natitira sa isang araw kasi umaga-hapon ang pakain ko.
             </p>
             <p>
                 (It hurts [my wallet] a bit since the price of the cat food I buy, Oc, is hefty, around Php 175 per kilo. Almost always, only a small amount of that kilo is left at the end of the day, since I feed them from morning until evening.)
             </p>
-            <h2 className={sectionStyles.quote}>"</h2>
+            <h2 className={sectionStyles.quotatiomarkRight}>"</h2>
         </article>
     </main>
     </>
   );
 };
 
-export default Conclusion;
+export default FurmiliarFaces;
 

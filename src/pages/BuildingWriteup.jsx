3d5daafe3abd4a7b288935375a198eb871bc69e6
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-13 00:54:24 +0800

    initial building article with building icon and write up

---
+++ src/pages/BuildingWriteup.jsx
@@ -0,0 +1,28 @@
+import buildings from "../data/locations.json";
+import styles from "../styles/BuildingWriteup.module.css";
+import bg from "../assets/images/Background.svg";
+
+export default function BuildingWriteup() {
+  const data = buildings[0];
+  return (
+    <main
+      className={styles.main}
+      style={{
+        background: `center / cover no-repeat url(${bg}), ${data.bgColor}`
+      }}
+    >
+      <section className={styles.writeup}>
+        <section className={styles.heading}>
+          <img src={`src/assets/images/${data.bldgImage}`} />
+          <section className={styles.titleCaption}>
+            <h1>{data.name}</h1>
+            <h2>{data.caption}</h2>
+          </section>
+        </section>
+        <p className={styles.content}>
+          {data.article}
+        </p>
+      </section>
+    </main>
+  );
+}

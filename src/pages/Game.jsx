a35f38cc2bda98f0c5ee52aa1cfaf3587aab8a6b
Abby Montayre <raphaelleabby.montayre@gmail.com>
2025-10-12 14:19:34 +0800

    Created Game.jsx, imported all assets, did basic Submit Button styling

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,20 @@
+import styles from "../styles/game.module.css";
+import Map from "../assets/images/game/map.svg";
+import { DndContext, closestCenter } from '@dnd-kit/core';
+
 function Game() {
   return (
     <>
+    <section className={styles}>
+      <h1>Find Meow way back home</h1>
+        <main className={styles.gamearea}>
+          <img src={Map}></img>
+          <h1 className={styles.submit}>Submit</h1>
+        </main>
+        <aside>
+          
+        </aside>
+    </section>
     </>
   );
 }

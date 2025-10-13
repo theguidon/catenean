7be671b0a700708c9cf4fdc6b9f25b9d95a8bd77
Abby Montayre <raphaelleabby.montayre@gmail.com>
2025-10-13 13:28:52 +0800

    Added Nav Section and Vant Logo

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,18 +1,26 @@
 import styles from "../styles/game.module.css";
 import Map from "../assets/images/game/map.svg";
+import VantLogo from "../assets/images/game/VantLogo.svg";
 import { DndContext, closestCenter } from '@dnd-kit/core';
 
 function Game() {
   return (
     <>
     <section className={styles}>
-      <h1>Find Meow way back home</h1>
-        <main className={styles.gamearea}>
-          <img src={Map}></img>
-          <h1 className={styles.submit}>Submit</h1>
+        <main>
+        <div className={styles.nav}>
+          <img src={VantLogo}></img>
+          <h1>Find Meow  <br/> way back home</h1>
+        </div>
+          <div className={styles.gamearea}>
+            <img src={Map}></img>
+            <h1 className={styles.submit}>Submit</h1>
+          </div>
         </main>
         <aside>
-          
+          <div>
+
+          </div>
         </aside>
     </section>
     </>

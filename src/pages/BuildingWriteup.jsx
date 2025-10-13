0645ee5cb8a75dc363b9de541dfd0752cf22af80
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-13 20:01:38 +0800

    desktop layout for building article

--- src/pages/BuildingWriteup.jsx
+++ src/pages/BuildingWriteup.jsx
@@ -1,6 +1,8 @@
 import buildings from "../data/locations.json";
 import styles from "../styles/BuildingWriteup.module.css";
 import bg from "../assets/images/Background.svg";
+import arrow from "../assets/images/Union.svg";
+import back from "../assets/images/Back.svg";
 
 export default function BuildingWriteup() {
   const data = buildings[0];
@@ -11,6 +13,14 @@ export default function BuildingWriteup() {
         background: `center / cover no-repeat url(${bg}), ${data.bgColor}`
       }}
     >
+      <img
+        src={back}
+        style={{
+          position: "absolute",
+          top: "5%",
+          right: "5%",
+        }}
+      />
       <section className={styles.writeup}>
         <section className={styles.heading}>
           <img src={`src/assets/images/${data.bldgImage}`} />
@@ -22,6 +32,26 @@ export default function BuildingWriteup() {
         <p className={styles.content}>
           {data.article}
         </p>
+      </section>
+      <section className={styles.cats}>
+        <article>
+          <img src={`src/assets/images/cats/${data.cats[0].pic}`} />
+          <p style={{ color: data.nameColor }} >{data.cats[0].name}</p>
+        </article>
+        <article>
+          <img src={`src/assets/images/cats/${data.cats[1].pic}`} />
+          <p style={{ color: data.nameColor }} >{data.cats[1].name}</p>
+        </article>
+        <section className={styles.navArrows}>
+          <section className={`${styles.link} ${styles.back}`}>
+            <img src={arrow} />
+            <p>{data.back.toUpperCase()}</p>
+          </section>
+          <section className={`${styles.link} ${styles.next}`}>
+            <p>{data.next.toUpperCase()}</p>
+            <img src={arrow} />
+          </section>
+        </section>
       </section>
     </main>
   );

043dfc1fd6ca26ca4833a888fea2c7444bba3d53
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-19 00:15:21 +0800

    switched to data mode react

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import ArticleMain from './pages/ArticleMain.jsx';
 import BuildingWriteup from "./pages/BuildingWriteup.jsx";
+import buildings from "./data/locations.json";
 import App from './App.jsx'
 import './index.css'
 
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: ArticleMain,
+  },
+  {
+    path: "location/:bldg",
+    Component: BuildingWriteup,
+    loader: ({ params }) => (buildings[params.bldg]),
+  },
+]);
 // consider making a 404 Page
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ArticleMain} />
-        <Route path="game" element={<>{/* <Game /> */}</>} />
-        <Route path="map" element={<>{/* <Map /> */}</>} />
-        <Route path="location/:bldg" element={BuildingWriteup} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

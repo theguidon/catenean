0fb8ffc437190d1993c5692b6f31113d9ebdf54b
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-19 00:02:27 +0800

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import ArticleMain from './pages/ArticleMain.jsx';
+import BuildingWriteup from "./pages/BuildingWriteup.jsx";
 import App from './App.jsx'
 import './index.css'
 
+// consider making a 404 Page
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ArticleMain} />
+        <Route path="game" element={<>{/* <Game /> */}</>} />
+        <Route path="map" element={<>{/* <Map /> */}</>} />
+        <Route path="location/:bldg" element={BuildingWriteup} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

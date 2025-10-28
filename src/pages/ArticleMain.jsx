3b4b628104a1cd09411f83059f10f05243904a6c
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-28 16:15:12 +0800

    initial desktop

--- src/pages/ArticleMain.jsx
+++ src/pages/ArticleMain.jsx
@@ -1,14 +1,28 @@
 import styles from "../styles/ArticleMain.module.css";
+import IntroDesktop from "../components/intro/IntroDesktop";
 import IntroMobile from "../components/intro/IntroMobile";
+import InLonelinessDesktop from "../components/mainarticle/InLonelinessDesktop";
+import ConclusionDesktop from "../components/mainarticle/ConclusionDesktop";
 import InLonelinessMobile from "../components/mainarticle/InLonelinessMobile";
 import InCaringMobile from "../components/mainarticle/InCaringMobile";
 import ComeAndGoMobile from "../components/mainarticle/ComeAndGoMobile";
 import ConclusionMobile from "../components/mainarticle/ConclusionMobile";
+import grassBg from "../assets/images/background-grass.svg";
 import { useMediaQuery } from "react-responsive";
 
 const ArticleMain = () => {
   const isDesktop = useMediaQuery({ query: "(min-width: 501px)" });
-  if (isDesktop) return (<p>Desktop coming soon</p>);
+  if (isDesktop) {
+    return (
+      <main className={styles.main}>
+        <div className={styles.grassLayer}>
+          <img src={grassBg} />
+        </div>
+        <IntroDesktop />
+        <ConclusionDesktop />
+      </main>
+    );
+  }
   return (
     <main className={styles.main}>
       <IntroMobile />

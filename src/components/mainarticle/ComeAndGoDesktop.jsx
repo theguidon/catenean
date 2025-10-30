2f58778751129ab8e2a3476047040c7bc041714d
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-10-30 23:19:19 +0800

    animated come and go

--- src/components/mainarticle/ComeAndGoDesktop.jsx
+++ src/components/mainarticle/ComeAndGoDesktop.jsx
@@ -1,31 +1,39 @@
-import { transform } from "motion";
+import { easeInOut, motion, stagger } from "motion/react";
 import cloud from "../../assets/images/cloud_desktop.svg";
 import paopao from "../../assets/images/cats/17.png";
 import styles from "../../styles/ComeAndGoDesktop.module.css";
 
 const ComeAndGoDesktop = () => {
+  const transition = { duration: 0.5, ease: easeInOut }
+  const parVariants = {
+    show: { color: "rgb(82,34,34,1)", transition },
+    hidden: { color: "rgb(82,34,34,0)", transition }
+  }
+
   return (
     <section className={styles.container}>
       <img src={cloud} className={styles.cloud} />
-      <section className={styles.content}>
-        <p
+      <motion.section initial="hidden" whileInView="show" transition={{ delayChildren: stagger(0.5) }} className={styles.content}>
+        <motion.p
+          variants={parVariants}
           style={{
             alignSelf: "flex-start",
             top: "5%",
             left: "12%"
           }}>
           Johnson recounts the day Pao-Pao was run over because of the &quot;reckless imprudence of drivers.&quot; He first heard the news of Pao-Pao&apos;s passing before his 5:00PM class. When he came back at 6:30PM, he recalled a crowd of around 70 students gathering by Schmitt Hall before they headed to the SOM Forest, where Pao-Pao was buried.
-        </p>
+        </motion.p>
         <img src={paopao} className={styles.paopao} />
-        <p
+        <motion.p
+          variants={parVariants}
           style={{
             alignSelf: "flex-end",
             right: "5%",
             bottom: "5%"
           }}>
           &quot;These small things which are part of our experience, [...] they&apos;re more important than we think. Sometimes, we only notice them when they&apos;re absent, [...] we realize how much we&apos;ve missed them&mdash;how much of the experience they are&quot;<br />Johnson says.
-        </p>
-      </section>
+        </motion.p>
+      </motion.section>
       <img src={cloud} className={styles.cloud} style={{ transform: "scaleX(-1)" }} />
     </section>
   );

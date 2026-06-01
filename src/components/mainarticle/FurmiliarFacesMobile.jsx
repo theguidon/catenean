035ec69b3544473b0ad34c91c7a0b9317ba3270e
Cheska Huang <julyannahuang@gmail.com>
2026-06-01 12:56:46 +0800

    schmitt section

--- src/components/mainarticle/FurmiliarFacesMobile.jsx
+++ src/components/mainarticle/FurmiliarFacesMobile.jsx
@@ -1,6 +1,10 @@
 import sectionStyles from "../../styles/FurmiliarFacesMobile.module.css";
 import styles from "../../styles/FurmiliarFacesDesktop.module.css";
 import CTC from "../../assets/images/location-ctc-som-building.svg";
+import Faura from "../../assets/images/buildings/no-shadow/faura.svg";
+import Arete from "../../assets/images/buildings/no-shadow/arete.svg";
+import Schmitt from "../../assets/images/buildings/no-shadow/schmitt.svg";
+import MVP from "../../assets/images/buildings/no-shadow/mvp.svg";
 import Grass from "../../assets/images/grass.svg";
 import Dongyan from "../../assets/images/cats/dongyan.png";
 import Barbie from "../../assets/images/cats/barbie.png";
@@ -94,6 +98,92 @@ const FurmiliarFacesMobile = () => {
           people. All of these residents are lovingly cared for by June,* a
           maintenance staff member of the Ateneo.
         </p>
+        <div
+          className={`${sectionStyles.heading} ${sectionStyles.section}`}
+          style={{
+            minHeight: "30vh",
+          }}
+        >
+          <img
+            src={MVP}
+            style={{
+              position: "absolute",
+              bottom: "13vw",
+              left: "20vw",
+              zIndex: "0",
+              width: "60%",
+            }}
+          ></img>
+          <img
+            src={Grass}
+            style={{ position: "absolute", bottom: "0", width: "101dvw" }}
+          />
+        </div>
+        <p>
+          In Areté, the cats do not shy away from showing off their unique
+          quirks. For instance, the late Ponpon was often kept inside a cage due
+          to his habit of staying near cars. He and George shared a playful
+          rivalry, with George’s bunso title once stolen by Ponpon before his
+          untimely passing.
+        </p>
+        <p>
+          A few steps away, in another corner of Areté, cateneans Tita Mel and
+          Jes remain elusive and difficult to spot, often found curled up
+          together in a perfect ball during their naps.
+        </p>
+        <div
+          className={`${sectionStyles.heading} ${sectionStyles.section}`}
+          style={{
+            minHeight: "30vh",
+          }}
+        >
+          <img
+            src={Arete}
+            style={{
+              position: "absolute",
+              bottom: "13vw",
+              left: "10vw",
+              zIndex: "0",
+              width: "80%",
+            }}
+          ></img>
+          <img
+            src={Grass}
+            style={{ position: "absolute", bottom: "0", width: "101dvw" }}
+          />
+        </div>
+        <p>
+          Over in Schmitt Hall, Hakaw and Maimai share the space in spite of
+          their contrasting personalities. According to Ateneans Guided and
+          Inspired by Love for Animals (AGILA) moderator and Chemistry
+          Department Chair Olivia Erin Buenafe, the extroverted Hakaw is
+          considered the “spiritual successor” to late Paopao, after inheriting
+          some of his endearing habits like opening windows. On the other hand,
+          introverted Maimai plays by her own terms, only interacting with
+          people whenever she deems fit and retreats inside buildings to relish
+          in her own company.
+        </p>
+        <div
+          className={`${sectionStyles.heading} ${sectionStyles.section}`}
+          style={{
+            minHeight: "35vh",
+          }}
+        >
+          <img
+            src={Schmitt}
+            style={{
+              position: "absolute",
+              bottom: "13vw",
+              left: "20vw",
+              zIndex: "0",
+              width: "60%",
+            }}
+          ></img>
+          <img
+            src={Grass}
+            style={{ position: "absolute", bottom: "0", width: "101dvw" }}
+          />
+        </div>
       </main>
     </>
   );

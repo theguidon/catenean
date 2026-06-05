81bf0c750ef5d48aab253e7408753c8a1120174a
Cheska Huang <julyannahuang@gmail.com>
2026-06-05 09:30:01 +0800

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import ArticleMain from './pages/ArticleMain.jsx';
+import ArticleMain from "./pages/ArticleMain.jsx";
 import BuildingWriteup from "./pages/BuildingWriteup.jsx";
 import buildings from "./data/locations.json";
-import Map from './pages/Map.jsx';
-import App from './App.jsx'
-import './index.css'
+import Map from "./pages/Map.jsx";
+import { Analytics } from "@vercel/analytics/next";
+import "./index.css";
 
 let router = createBrowserRouter([
   {
@@ -15,17 +15,18 @@ let router = createBrowserRouter([
   },
   {
     path: "/map",
-    Component: Map
+    Component: Map,
   },
   {
     path: "location/:bldg",
     Component: BuildingWriteup,
-    loader: ({ params }) => (buildings[params.bldg]),
+    loader: ({ params }) => buildings[params.bldg],
   },
 ]);
 // consider making a 404 Page
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
-)
+);
